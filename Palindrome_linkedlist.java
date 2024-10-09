ee595cf1418b81e4c4653cf48b01e46ff9f3ec17
Neharithanya <rithanyaneha2@gmail.com>
2024-10-09 15:11:49 +0530

    Create Palindrome_linkedlist.java

---
+++ Palindrome_linkedlist.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList llist= new LinkedList(); 
+        int a1=sc.nextInt();
+        Node head= new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) 
+        {
+            int a = sc.nextInt(); 
+            llist.addToTheLast(new Node(a));
+        }
+            
+        Solution A = new Solution();
+        A.checkLinkedListPalindrome(llist.head);
+        
+    }
+}
+
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    
+class LinkedList
+{
+    Node head;  
+    Node tail;
+    public void addToTheLast(Node node) 
+    {
+      if (head == null) 
+      {
+       head = node;
+       tail = node;
+      } 
+      else 
+      {
+       tail.next = node;
+       tail = node;
+      }
+    }
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+    
+}
+
+
+class Solution
+{
+    static void checkLinkedListPalindrome(Node head)
+    {
+        List<Integer> list=new ArrayList<>();
+        while(head!=null){
+            list.add(head.data);
+            head=head.next;
+             }
+        int left=0;
+        int right=list.size()-1;
+        while(left<right && list.get(left)==list.get(right)){
+            left++;
+            right--;
+          }
+          if(left>=right){
+              System.out.print("YES");
+          }else{
+              System.out.print("NO");
+          }
+
+        
+        
+        
+          
+          
+    }
+    
+}
